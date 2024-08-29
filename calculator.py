af987e327a3e22a57e2b680f4ad8f521435df8ab
Likithapathuri25 <likithapathuri@gmail.com>
2024-08-29 20:09:47 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+operator=input("enter the operator(+,-,*,/):")
+if operator == "+":
+      print("sum=:",a+b)
+elif operator == "-":
+    print("diffference=",a-b)
+elif operator == "*":
+    print("multiply=",a*b)
+elif operator == "/":
+    print("division=",a/b)
+
